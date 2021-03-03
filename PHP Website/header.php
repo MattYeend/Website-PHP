fa19e13a5b584a6b9c5eb38d2ba08823f5b72f3f
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-03 09:28:55 +0000

    Initial Commit

---
+++ PHP Website/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>TTA Consulting</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<!-- Page header -->
+	<table width="100%" cellpadding="12" cellspacing="0" border="0">
+		<tr bgcolor="black">
+			<td align="left"><img src="#" alt="TTA logo" height="70" width="70"></td>
+			<td><h1>TTA Consulting</h1></td>
+			<td align="left"><img src="#" alt="TTA logo" height="70" width="70"></td>
+		</tr>
+	</table>
+	<!-- menu -->
+	<table width="100%" bgcolor="white" cellpadding="4" cellspacing="4">
+		<tr>
+			<td width="25%">
+				<img src="#" alt="img1" height="20" width="20">
+				<span class="menu">Home</span>
+			</td>
+			<td width="25%">
+				<img src="#" alt="img2" height="20" width="20">
+				<span class="menu">Contact</span>
+			</td>
+			<td width="25%">
+				<img src="#" alt="img3" height="20" width="20">
+				<span class="menu">Services</span>
+			</td>
+			<td width="25%">
+				<img src="#" alt="img4" height="20" width="20">
+				<span class="menu">Site Map</span>
+			</td>
+		</tr>
+	</table>
---
+++ PHP Website/home.php
@@ -0,0 +1,11 @@
+<?php
+	require('header.php');
+?>
+	<!-- Page content -->
+	<p>Welcome to the home of TTA Consulting.
+		Please take some time to get to know us.</p>
+	<p>We specialize in serving your business needs
+		and hope to hear from you soon.</p>
+<?php
+	require('footer.php');
+?>
